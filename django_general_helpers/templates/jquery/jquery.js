79397f3c73a259b844bea373c84d6c710a22f19c
Daniel C <333222@gmail.com>
2010-10-16 20:54:29 +0200

    Upgrade: jQuery 1.4.2 -> 1.4.3

--- django_general_helpers/templates/jquery/jquery.js
+++ django_general_helpers/templates/jquery/jquery.js
@@ -2,7 +2,7 @@
 Load the jQuery lib and plugins passed with {{ jquery_plugins }}.
 {% endcomment %}
 
-{% include 'jquery/jquery-1.4.2.min.js' %}
+{% include 'jquery/jquery-1.4.3.min.js' %}
 
 {% for jquery_plugin in jquery_plugins %}
     {% captureas plugin_path %}jquery/plugins/{{ jquery_plugin }}.js{% endcaptureas %}
