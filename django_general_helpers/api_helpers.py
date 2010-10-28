b08cc59a5be5aa38bfaafbb94ce039890b0af3ff
Daniel C <333222@gmail.com>
2010-10-28 15:38:35 +0200

    add module api_helpers

---
+++ django_general_helpers/api_helpers.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+from django.http import HttpResponse
+
+try:
+    import json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        # For Google AppEngine
+        from django.utils import simplejson as json
+
+def sha1(string):
+    import hashlib
+
+    h = hashlib.new('sha1')
+    h.update(string)
+    return h.hexdigest()
+
+def json_response(response_obj):
+    return HttpResponse(json.dumps(response_obj, default=lambda obj: obj.__dict__), mimetype="application/json")
+
+def string_response(string):
+    return HttpResponse(string)
+
+def error_response(error, description, status_code=200):
+    """The general structure for error responses"""
+
+    from django.http import HttpResponseBadRequest
+
+    response_obj = json.dumps({'error': error, 'description': description})
+
+    if status_code == 200:
+        return HttpResponse(response_obj, mimetype="application/json")
+    elif status_code == 400:
+        return HttpResponseBadRequest(response_obj, mimetype="application/json")
+
+def require_get_request(view):
+    """Decorator for views that returns HttpResponseNotAllowed(['GET']) if
+    the http method of the request isn't GET.
+
+    Otherwise we simply call the decorated view.
+    """
+
+    def check_get_request(request, *args, **kwargs):
+        if request.method != 'GET':
+            from django.http import HttpResponseNotAllowed
+
+            return HttpResponseNotAllowed(['GET'])
+
+        return view(request, *args, **kwargs)
+
+    return check_get_request
+
+def require_post_request(view):
+    """Decorator for views that returns HttpResponseNotAllowed(['POST']) if
+    the http method of the request isn't POST.
+
+    Otherwise we simply call the decorated view.
+    """
+
+    def check_post_request(request, *args, **kwargs):
+        if request.method != 'POST':
+            from django.http import HttpResponseNotAllowed
+
+            return HttpResponseNotAllowed(['POST'])
+
+        return view(request, *args, **kwargs)
+
+    return check_post_request
+
+def get_name_identifier(name):
+    import re
+    return re.sub('\s+', '.', name.lower())
