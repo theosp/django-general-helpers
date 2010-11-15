8884f71eca4a1ffd0965b6417f73408eb6588930
Daniel C <333222@gmail.com>
2010-11-15 15:41:01 +0200

    Don't include jquery's code in the jquery.js template, use google's libraries api to get it

--- django_general_helpers/templates/jquery/jquery.js
+++ django_general_helpers/templates/jquery/jquery.js
@@ -2,8 +2,6 @@
 Load the jQuery lib and plugins passed with {{ jquery_plugins }}.
 {% endcomment %}
 
-{% include 'jquery/jquery-1.4.3.min.js' %}
-
 {% for jquery_plugin in jquery_plugins %}
     {% captureas plugin_path %}jquery/plugins/{{ jquery_plugin }}.js{% endcaptureas %}
     {% include plugin_path %}
